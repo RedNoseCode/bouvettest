29cf42576fe3979a2e359f59e59a4069a37d0c31
RedNoseCode <edgargrane99@gmail.com>
2026-03-17 14:55:40 +0100

    changes to main.tf and with claude

---
+++ main.tf
@@ -0,0 +1,25 @@
+module "github_runners" {
+  source = "github.com/patrickthor/github-runners?ref=v3.0.0"
+
+  subscription_id = var.subscription_id
+  workload        = var.workload
+  environment     = var.environment
+  instance        = var.instance
+  location        = var.location
+  github_org      = var.github_org
+  github_repo     = var.github_repo
+
+  runner_workload_roles = var.runner_workload_roles
+}
+
+output "acr_login_server" {
+  value = module.github_runners.acr_login_server
+}
+
+output "function_app_name" {
+  value = module.github_runners.function_app_name
+}
+
+output "resource_group_name" {
+  value = module.github_runners.resource_group_name
+}
